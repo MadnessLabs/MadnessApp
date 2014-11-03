973eaf1adb3a47a877922e4c7798268348892362
unknown <bobby.johnson@Popcorn245-PC.ad.metrogistics.com>
2014-11-03 11:59:45 -0600

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var uglify = require('gulp-uglify');
+var install = require("gulp-install");
+var runSequence = require('run-sequence');
+var livereload = require('gulp-livereload');
+
+var paths = {
+    html: [
+        './public/html/*.html',
+        './public/index.html'
+    ],
+    less: [
+        './src/less/**/*.less'
+    ],
+    js: [
+        './bower_components/jquery/dist/jquery.js',
+        './bower_components/bootstrap/dist/js/bootstrap.js',
+        './bower_components/angular/angular.js',
+        './bower_components/angular-route/angular-route.js',
+        './bower_components/angular-animate/angular-animate.js',
+        './bower_components/angular-ui-bootstrap-bower/ui-bootstrap.js',
+        './src/js/lib/**.js',
+        './src/js/app.js',
+        './src/js/router.js',
+        './src/js/ctrl/**.js',
+        './src/js/fctry/**.js'
+    ],
+    font: [
+        './bower_components/bootstrap/dist/fonts/**',
+        './bower_components/fontawesome/fonts/**'
+    ]
+};
+
+gulp.task('default', function(){
+    runSequence(['less-watch', 'js-watch'], 'watch');
+});
+
+gulp.task('build', ['less', 'js']);
+
+gulp.task('less-watch', function() {
+    gulp.src('./src/less/app.less')
+        .pipe(less())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./public/css'))
+        .pipe(livereload());
+});
+
+gulp.task('less', function() {
+    gulp.src('./src/less/app.less')
+        .pipe(less())
+        .pipe(gulp.dest('./public/css'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js-watch', function() {
+    return gulp.src(paths.js)
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(rename('build.min.js'))
+        .pipe(uglify({
+            "mangle": false
+        }))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(livereload());
+});
+
+gulp.task('js', function() {
+    return gulp.src(paths.js)
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./public/js'))
+        .pipe(rename('build.min.js'))
+        .pipe(uglify({
+            "mangle": false
+        }))
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('watch', function() {
+    var server = livereload();
+
+    gulp.watch(paths.html).on('change', function(file){
+        livereload(server).changed(file.path);
+    });
+    gulp.watch(paths.less, ['less-watch']);
+    gulp.watch(paths.js, ['js-watch']);
+});
+
+gulp.task('copy-fonts', function() {
+    return gulp.src(paths.font)
+        .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('bower', function() {
+    return gulp.src(['./bower.json'])
+        .pipe(install());
+});
+
+gulp.task('install', function() {
+    runSequence(
+        'bower', ['copy-fonts'], ['less', 'js']
+    );
+});
